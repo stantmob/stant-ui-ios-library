e8dd02f7f4f0158d3259f87b3e282a431689e68f
Mac Mini Novo <macmininew@Mac-mini-de-Mac.local>
2019-05-21 09:23:47 -0300

    Initial commit

---
+++ Example/StantUiIosLibrary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StantUiIosLibrary
+//
+//  Created by liuvasconcelos on 05/21/2019.
+//  Copyright (c) 2019 liuvasconcelos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
