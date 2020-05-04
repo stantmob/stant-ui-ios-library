d4aa96258b6b792fd77b0056acd09f59eae39e88
Leandro97 <leandro97.martins@gmail.com>
2020-05-04 16:21:50 -0300

    feature/INS-445-quantity-screen Some corrections in cpmponent names Signed-off-by: Leandro97 <leandro97.martins@gmail.com>

--- StantUiIosLibrary/Classes/Utils/FloatExtension.swift
+++ StantUiIosLibrary/Classes/Utils/FloatExtension.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Leandro Martins on 09/01/20.
 //
-extension Float {
+public extension Float {
     var cleanValue: String {
         return self.truncatingRemainder(dividingBy: 1) <= 0.00001 ? String(format: "%.0f", self) : String(format: "%.1f", self)
     }
