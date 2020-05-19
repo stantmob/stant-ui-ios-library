0ab0c734754fda0514b1ffe453e69231c76b8e1e
Leandro97 <leandro97.martins@gmail.com>
2020-05-19 18:16:48 -0300

    feature/INS-449-siff-crud-screen-integration Change in roundWith function Signed-off-by: Leandro97 <leandro97.martins@gmail.com>

--- StantUiIosLibrary/Classes/Utils/FloatExtension.swift
+++ StantUiIosLibrary/Classes/Utils/FloatExtension.swift
@@ -8,4 +8,9 @@ public extension Float {
     var cleanValue: String {
         return self.truncatingRemainder(dividingBy: 1) <= 0.00001 ? String(format: "%.0f", self) : String(format: "%.1f", self)
     }
+    
+    func roundWith(decimalPlaces: Int) -> Float {
+        let power = Float(pow(Double(10), Double(decimalPlaces)))
+        return (self * power).rounded() / power
+    }
 }
