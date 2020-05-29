b15ab0316691a722b9409ab01a3cab15eb9c2239
Leandro97 <leandro97.martins@gmail.com>
2020-05-29 12:28:48 -0300

    feature/INS-256-siff-detail-components Fix in tabview Signed-off-by: Leandro97 <leandro97.martins@gmail.com>

--- StantUiIosLibrary/Classes/Menus/ServiceInspectionFormFilledDetailMenu/TabView/CustomTabView.swift
+++ StantUiIosLibrary/Classes/Menus/ServiceInspectionFormFilledDetailMenu/TabView/CustomTabView.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 class CustomTabView: UIView {
-    var stackView:        UIStackView?
-    var formButton:       CustomTabViewButton?
-    var teamButton:       CustomTabViewButton?
-    var attachmentButton: CustomTabViewButton?
-    var delegate:         CustomTabViewDelegate?
+    var collectionView: UICollectionView?
+    var delegate:       CustomTabViewDelegate?
+    
+    let cellTitles = [AppStrings.service_inspection_form_filled_details_form_title,
+                      AppStrings.service_inspection_form_filled_details_team_title,
+                      AppStrings.service_inspection_form_filled_details_attachments_title]
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -20,75 +21,65 @@ class CustomTabView: UIView {
     
     init() {
         super.init(frame: .zero)
-        configureStackView()
+        configureCollectionView()
     }
     
-    func configureStackView() {
-        stackView              = UIStackView()
-        guard let stackView    = stackView else { return }
-        stackView.alignment    = .fill
-        stackView.distribution = .fillEqually
-        stackView.axis         = .horizontal
+    func configureCollectionView() {
+        let layout                     = UICollectionViewFlowLayout()
+        collectionView                 = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        guard let collectionView       = collectionView else { return }
+        collectionView.backgroundColor = .white
+        collectionView.delegate        = self
+        collectionView.dataSource      = self
         
-        self.addSubview(stackView)
-        stackView.fillSuperView()
+        collectionView.register(CustomTabViewCell.self, forCellWithReuseIdentifier: "cellID")
         
-        configureFormButton()
-        configureTeamButton()
-        configureAttachmentButton()
+        self.addSubview(collectionView)
+        collectionView.fillSuperView()
+        collectionView.selectItem(at:             IndexPath(row: 0, section: 0),
+                                  animated:       true,
+                                  scrollPosition: .left)
+    }
+}
+
+extension CustomTabView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
     }
     
-    func configureFormButton() {
-        formButton            = CustomTabViewButton(frame: CGRect(x: 0, y: 0, width: 109, height: 38))
-        guard let formButton  = formButton else { return }
-        guard let stackView   = stackView else { return }
+    func collectionView(_ collectionView:        UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        formButton.configure(title: AppStrings.service_inspection_form_filled_details_form_title)
-        formButton.toggleSelection()
-        formButton.addTarget(self, action: #selector(goToFormTabView), for: .touchUpInside)
-        stackView.addArrangedSubview(formButton)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID",
+                                                      for:                 indexPath) as! CustomTabViewCell
         
+        cell.label?.text = cellTitles[indexPath.row]
+        return cell
     }
     
-    @objc func goToFormTabView() {
-        formButton?.tag       = 1
-        teamButton?.tag       = 0
-        attachmentButton?.tag = 0
-        delegate?.goToFormTabView()
-    }
-    
-    func configureTeamButton() {
-        teamButton            = CustomTabViewButton(frame: CGRect(x: 0, y: 0, width: 109, height: 38))
-        guard let teamButton  = teamButton else { return }
-        guard let stackView   = stackView else { return }
+    func collectionView(_ collectionView:            UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath:     IndexPath) -> CGSize {
         
-        teamButton.configure(title: AppStrings.service_inspection_form_filled_details_team_title)
-        teamButton.addTarget(self, action: #selector(goToTeamTabView), for: .touchUpInside)
-        stackView.addArrangedSubview(teamButton)
+        return CGSize(width:  collectionView.frame.width / 3,
+                      height: collectionView.frame.height)
     }
     
-    @objc func goToTeamTabView() {
-        formButton?.tag       = 0
-        teamButton?.tag       = 1
-        attachmentButton?.tag = 0
-        delegate?.goToTeamTabView()
+    func collectionView(_ collectionView:                            UICollectionView,
+                        layout collectionViewLayout:                 UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
-    func configureAttachmentButton() {
-        attachmentButton            = CustomTabViewButton(frame: CGRect(x: 0, y: 0, width: 109, height: 38))
-        guard let attachmentButton  = attachmentButton else { return }
-        guard let stackView         = stackView else { return }
-        
-        attachmentButton.configure(title: AppStrings.service_inspection_form_filled_details_attachments_title)
-        attachmentButton.addTarget(self, action: #selector(goToAttachmentTabView), for: .touchUpInside)
-        stackView.addArrangedSubview(attachmentButton)
-    }
-    
-    @objc func goToAttachmentTabView() {
-        formButton?.tag       = 0
-        teamButton?.tag       = 0
-        attachmentButton?.tag = 1
-        delegate?.goToAttachmentTabView()
+    func collectionView(_ collectionView:          UICollectionView,
+                        didSelectItemAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            delegate?.goToFormTabView()
+        } else if indexPath.row == 1 {
+            delegate?.goToTeamTabView()
+        } else {
+            delegate?.goToAttachmentTabView()
+        }
     }
 }
 
