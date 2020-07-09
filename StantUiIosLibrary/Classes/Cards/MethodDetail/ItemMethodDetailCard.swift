a62436d5fefe820a0f8a3d80a87f41651f1126f5
Renato <renato.vieira@stant.com.br>
2020-07-09 18:04:37 -0300

    Added separator, switch and buttons approve and repprove

--- StantUiIosLibrary/Classes/Cards/MethodDetail/ItemMethodDetailCard.swift
+++ StantUiIosLibrary/Classes/Cards/MethodDetail/ItemMethodDetailCard.swift
@@ -11,10 +11,14 @@ public class ItemMethodDetailCard: UIView {
     public var view: UIView?
     public var descriptionLabel: UILabel?
     public var detailDescriptionLabel: UILabel?
-    public var separatorView: UIView?
+    public var upperSeparator: UIView?
+    public var lowerSeparator: UIView?
     public var noApplicableView: UIView?
     public var noApplicableLabel: UILabel?
-    
+    public var noApllicableSwitch: UISwitch?
+    public var approveButton: UIButton?
+    public var repproveButton: UIButton?
+    public var inspectionLabel: UILabel?
     
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -25,18 +29,25 @@ public class ItemMethodDetailCard: UIView {
     }
     
     public func configure() {
+        self.removeSubviews()
+        addShadow()
         configureView()
         configureDescriptionLabel()
         configureDetailDescriptionLabel()
-        configureSeparatorView()
+        configureUpperNoApplicableViewSeparator()
         configureNoApplicableView()
         configureNoApplicableLabel()
+        configureNoApllicableSwitch()
+        configureLowerNoApplicableViewSeparator()
+        configureApprovedButton()
+        configureRepprovedButton()
+        configureInspectionLabel()
     }
     
     fileprivate func configureView() {
         view                 = UIView()
         guard let view       = view else { return }
-        view.backgroundColor = .yellowLightStant
+        view.backgroundColor = .white
         
         self.setNeedsLayout()
         self.layoutIfNeeded()
@@ -45,7 +56,7 @@ public class ItemMethodDetailCard: UIView {
         view.anchor(leading:  self.leadingAnchor,
                     trailing: self.trailingAnchor,
                     size:     CGSize(width:  360,
-                                     height: 237))
+                                     height: 260))
     }
     
     fileprivate func configureDescriptionLabel() {
@@ -65,7 +76,7 @@ public class ItemMethodDetailCard: UIView {
         detailDescriptionLabel = UILabel()
         guard let detailDescriptionLabel = detailDescriptionLabel else { return }
         detailDescriptionLabel.backgroundColor = .white
-        detailDescriptionLabel.textColor = .darkGray
+        detailDescriptionLabel.textColor = .darkGrayStant
         detailDescriptionLabel.text = "Observar a eliminação de qualquer foco de umidade de modo que a superfície apresente-se seca quando da execução da pintura"
         detailDescriptionLabel.font = .systemFont(ofSize: 11)
         detailDescriptionLabel.numberOfLines = 3
@@ -75,14 +86,24 @@ public class ItemMethodDetailCard: UIView {
         detailDescriptionLabel.anchor(top: descriptionLabel?.bottomAnchor, leading: self.leadingAnchor, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: 5, left: 16, bottom: 0, right: 16))
     }
     
-    fileprivate func configureSeparatorView() {
-        separatorView = UIView()
-        guard let separatorView = separatorView else { return }
-        separatorView.backgroundColor = .gray
+    fileprivate func configureUpperNoApplicableViewSeparator() {
+        upperSeparator = UIView()
+        guard let  upperSeparator = upperSeparator else { return }
+        upperSeparator.backgroundColor = .grayStant
         
-        self.addSubview(separatorView)
+        self.addSubview(upperSeparator)
         
-        separatorView.anchor(top: detailDescriptionLabel?.bottomAnchor, leading: self.leadingAnchor, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: 16, left: 0, bottom: 0, right: 0), size: CGSize(width: self.frame.width, height: 0.5))
+        upperSeparator.anchor(top: detailDescriptionLabel?.bottomAnchor, leading: self.leadingAnchor, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: 16, left: 0, bottom: 0, right: 0), size: CGSize(width: 0, height: 1))
+    }
+    
+    fileprivate func configureLowerNoApplicableViewSeparator() {
+        lowerSeparator = UIView()
+        guard let  lowerSeparator = lowerSeparator else { return }
+        lowerSeparator.backgroundColor = .grayStant
+        
+        self.addSubview(lowerSeparator)
+        
+        lowerSeparator.anchor(top: noApplicableLabel?.bottomAnchor, leading: self.leadingAnchor, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: 13, left: 0, bottom: 0, right: 0), size: CGSize(width: 0, height: 1))
     }
     
     fileprivate func configureNoApplicableView() {
@@ -92,7 +113,7 @@ public class ItemMethodDetailCard: UIView {
         
         self.addSubview(noApplicableView)
         
-        noApplicableView.anchor(top: separatorView?.bottomAnchor, leading: self.leadingAnchor, trailing: self.trailingAnchor, size: CGSize(width: self.frame.width, height: 48))
+        noApplicableView.anchor(top: upperSeparator?.bottomAnchor, leading: self.leadingAnchor, trailing: self.trailingAnchor, size: CGSize(width: self.frame.width, height: 48))
     }
     
     fileprivate func configureNoApplicableLabel() {
@@ -107,4 +128,71 @@ public class ItemMethodDetailCard: UIView {
         
         noApplicableLabel.anchor(top: noApplicableView?.topAnchor, leading: noApplicableView?.leadingAnchor, bottom: noApplicableView?.bottomAnchor, padding: UIEdgeInsets(top: 15, left: 16, bottom: 14, right: 0))
     }
+    
+    fileprivate func configureNoApllicableSwitch() {
+        noApllicableSwitch = UISwitch()
+        guard let noApllicableSwitch = noApllicableSwitch else { return }
+        
+        self.addSubview(noApllicableSwitch)
+        
+        noApllicableSwitch.anchor(top: noApplicableView?.topAnchor, bottom: noApplicableView?.bottomAnchor, trailing: noApplicableView?.trailingAnchor, padding: UIEdgeInsets(top: 15, left: 0, bottom: 14, right: 16), size: CGSize(width: 51, height: 31))
+    }
+    
+    fileprivate func configureApprovedButton() {
+        approveButton = UIButton()
+        guard let approveButton = approveButton else { return }
+        approveButton.titleLabel?.font = .systemFont(ofSize: 12, weight: .bold)
+        approveButton.backgroundColor = .greenStant
+        approveButton.layer.cornerRadius = 3
+        
+        self.addSubview(approveButton)
+        approveButton.anchor(top: noApplicableView?.bottomAnchor, leading: self.leadingAnchor, padding: UIEdgeInsets(top: 16, left: 34, bottom: 0 , right: 0), size: CGSize(width: 140, height: 50))
+        
+        approveButton.setTitle("APROVAR", for: .normal)
+        approveButton.setTitleColor(.white, for: .normal)
+    }
+    
+    fileprivate func configureRepprovedButton() {
+        repproveButton = UIButton()
+        guard let repproveButton = repproveButton else { return }
+        repproveButton.titleLabel?.font = .systemFont(ofSize: 12, weight: .bold)
+        repproveButton.backgroundColor = .redLightStant
+        repproveButton.layer.cornerRadius = 3
+        
+        self.addSubview(repproveButton)
+        repproveButton.anchor(top: noApplicableView?.bottomAnchor, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: 16, left: 0, bottom: 0 , right: 34), size: CGSize(width: 140, height: 50))
+        
+        repproveButton.setTitle("REPROVAR", for: .normal)
+        repproveButton.setTitleColor(.white, for: .normal)
+    }
+    
+    fileprivate func configureInspectionLabel() {
+        inspectionLabel = UILabel()
+        guard let inspectionLabel = inspectionLabel else { return }
+        inspectionLabel.backgroundColor = .white
+        inspectionLabel.font = .systemFont(ofSize: 12, weight: .bold)
+        inspectionLabel.text = "Inspeções"
+        inspectionLabel.textColor = .darkGrayStant
+        inspectionLabel.textAlignment = .center
+        
+        self.addSubview(inspectionLabel)
+        inspectionLabel.anchor(top: approveButton?.bottomAnchor, leading: self.leadingAnchor, bottom: view?.bottomAnchor, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: 15, left: 152, bottom: 8, right: 152))
+    }
+    
+    fileprivate func addShadow() {
+        self.layer.applySketchShadow(color:  .shadowStant,
+                                     alpha:  0.09,
+                                     x:      0,
+                                     y:      3,
+                                     blur:   8,
+                                     spread: 0)
+    }
+    
+    public func makeShadow() {
+        self.layer.shadowPath    = UIBezierPath(rect: self.bounds).cgPath
+        self.layer.shadowRadius  = 4
+        self.layer.shadowOffset  = CGSize(width: 0, height: 3)
+        self.layer.shadowOpacity = 0.1
+    }
+    
 }
